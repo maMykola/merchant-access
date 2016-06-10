5d72cf7957f7560f166d42532925a09fc64282bd
Michael Strohyi <michael.strohyi@gmail.com>
2016-06-10 15:37:59 +0300

    [Core]: Move helper function for email validation into separate file

--- include/core.php
+++ include/core.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/constants.php';
 require_once ROOT_DIR . 'vendor/autoload.php';
 require_once INCLUDE_DIR . 'config.php';
+require_once INCLUDE_DIR . 'helper_func.php';
 require_once INCLUDE_DIR . 'db_func.php';
 
 
