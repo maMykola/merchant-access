62690147aec811e2d9efcdb5ed28d4e0f31bccc5
Michael Strohyi <michael.strohyi@gmail.com>
2016-06-17 00:24:32 +0300

    [Config]: Added const with server address for email confirmation

--- include/config.example.php
+++ include/config.example.php
@@ -1,12 +1,4 @@
 <?php
-$db_config = [
-    'dbName'=>'merchant_access',
-    'dbLogin'=>'LOGIN',
-    'dbPassword'=>'PASSWORD',
-    'host'=>'localhost',
-    ];
-
-
 
 defined("DB_CONFIG_NAME") || define("DB_CONFIG_NAME", 'merchant_access');
 defined("DB_CONFIG_LOGIN") || define("DB_CONFIG_LOGIN", 'LOGIN');
@@ -15,3 +7,5 @@ defined("DB_CONFIG_HOST") || define("DB_CONFIG_HOST", 'localhost');
 
 defined("GOOGLE_CAPTCHA_SITEKEY") || define("GOOGLE_CAPTCHA_SITEKEY", 'SITEKEY');
 defined("GOOGLE_CAPTCHA_SECRETKEY") || define("GOOGLE_CAPTCHA_SECRETKEY", 'SECRETKEY');
+
+defined("SIGNUP_SERVER") || define("SIGNUP_SERVER", 'www.signupserver.com');
